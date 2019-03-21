b2a1fce8117d63e2d110f07f671514c9df47cd20
vnovick <vnovick@gmail.com>
2019-03-21 22:06:15 +0200

    finalizing the app

--- src/router.js
+++ src/router.js
@@ -1,10 +1,12 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import Login from "./views/Login.vue";
+import { TokenService } from "./services/storage";
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
@@ -14,13 +16,37 @@ export default new Router({
       component: Home
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      path: "/chat/:userId",
+      component: Home,
+      props: true
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login,
+      meta: {
+        public: true
+      }
     }
   ]
 });
+
+router.beforeEach((to, from, next) => {
+  const isPublic = to.matched.some(record => record.meta.public);
+  const onlyWhenLoggedOut = to.matched.some(
+    record => record.meta.onlyWhenLoggedOut
+  );
+  const loggedIn = !!TokenService.getToken();
+  if (!isPublic && !loggedIn) {
+    return next({
+      path: "/login",
+      query: { redirect: to.fullPath }
+    });
+  }
+  if (loggedIn && onlyWhenLoggedOut) {
+    return next("/");
+  }
+  next();
+});
+
+export default router;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,81 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ChatWrapper :userId="userId" :username="username"/>
+    <footer class="App-footer">
+          <div class="hasura-logo">
+            <img src="https://graphql-engine-cdn.hasura.io/img/powered_by_hasura_black.svg" @click="windowOpen" alt="Powered by Hasura"/>
+            &nbsp; | &nbsp;
+            <a href="/console" target="_blank" rel="noopener noreferrer">
+              Backend
+            </a>
+            &nbsp; | &nbsp;
+            <a href="https://github.com/hasura/graphql-engine/tree/master/community/examples/realtime-chat" target="_blank" rel="noopener noreferrer">
+              Source
+            </a>
+            &nbsp; | &nbsp;
+            <a href="https://blog.hasura.io/building-a-realtime-chat-app-with-graphql-subscriptions-d68cd33e73f" target="_blank" rel="noopener noreferrer">
+              Blogpost
+            </a>
+          </div>
+          <div class="footer-small-text"><span>(The database resets every 24 hours)</span></div>
+              <button
+            class="btn btn-outline-secondary"
+            type="submit"
+            @click="logOut"
+          >
+            Log Out
+          </button>
+        </footer>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import ChatWrapper from "@/components/ChatWrapper.vue";
+import { TokenService } from '../services/storage';
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    ChatWrapper
+  },
+  props:{
+    userId: {
+      type: String,
+      default() {
+        const savedData = JSON.parse(TokenService.getToken())
+        return savedData && `${savedData.userId}`
+      }
+    },
+    username: {
+      type: String,
+      default(){
+        const savedData = JSON.parse(TokenService.getToken())
+        return this.$route.query.username || savedData && savedData.username
+      }
+    }
+  },
+  methods: {
+    windowOpen(){
+      window.open("https://hasura.io")
+    },
+    logOut() {
+      TokenService.removeToken()
+      this.$router.go();
+    }
+  },
+  created(){
+    setInterval(
+      async () => {
+        await this.$apollo.mutate({
+          mutation: require('../graphql/emitOnlineEvent.gql'),
+          variables: {
+            userId: this.userId
+          }
+        })
+      },
+      3000
+    )
   }
 };
 </script>
